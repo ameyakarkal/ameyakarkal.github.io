30ae1796eff70f867d1105a690052d3b4f83e2be
akarkal <akarkal@campuslabs.com>
2021-11-19 00:07:32 -0500

    using hugo workflow

--- index.md
+++
@@ -1,6 +0,0 @@
----
-layout : landing
-description: ameya karkal personal home page
----
-
-Welcome
--- run.sh
+++
@@ -1,2 +0,0 @@
-bundle install && bundle exec jekyll serve --watch --host 0.0.0.0
-#open http://localhost:4000
